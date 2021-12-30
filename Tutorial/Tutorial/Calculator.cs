fb249ae78953dddd101f3dbf2ea0304a2b0d782c
Yash-1511 <yashp3020@gmail.com>
2021-12-30 23:33:48 +0530

    committed

---
+++ Tutorial/Tutorial/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutorial
+{
+    public class Calculator
+    {
+        public static int add(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
